b507455d71da12013a593bdc970354a090fa001b
Marcin Scislowski <cokolwiek@c.com>
2022-01-22 20:23:40 +0100

    Poprawiony błąd z reallociem w points.c

--- lmp10/points.c
+++ lmp10/points.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 
 static int
-realloc_pts_failed (points_t * pts, int size)
+realloc_pts_failed(points_t *pts, int size)
 {
-  return realloc (pts->x, size * sizeof *pts->x) == NULL
-    || realloc (pts->y, size * sizeof *pts->y) == NULL;
+  pts->x = realloc(pts->x, size * sizeof(double));
+  pts->y = realloc(pts->y, size * sizeof(double));
+  return pts->x == NULL || pts->y == NULL;
 }
 
-int
-read_pts_failed (FILE * inf, points_t * pts)
+int read_pts_failed(FILE *inf, points_t *pts)
 {
   int size;
   double x, y;
 
-  if (pts->n == 0) {
-    pts->x = malloc (100 * sizeof *pts->x);
+  if (pts->n == 0)
+  {
+    pts->x = malloc(100 * sizeof *pts->x);
     if (pts->x == NULL)
       return 1;
-    pts->y = malloc (100 * sizeof *pts->y);
-    if (pts->y == NULL) {
-      free (pts->x);
+    pts->y = malloc(100 * sizeof *pts->y);
+    if (pts->y == NULL)
+    {
+      free(pts->x);
       return 1;
     }
     size = 100;
@@ -28,20 +30,21 @@ read_pts_failed (FILE * inf, points_t * pts)
   else
     size = pts->n;
 
-  while (fscanf (inf, "%lf %lf", &x, &y) == 2) {
+  while (fscanf(inf, "%lf %lf", &x, &y) == 2)
+  {
     pts->x[pts->n] = x;
     pts->y[pts->n] = y;
     pts->n++;
-    if (!feof (inf) && pts->n == size) {
-      if (realloc_pts_failed (pts, 2 * size))
+    if (!feof(inf) && pts->n == size)
+    {
+      if (realloc_pts_failed(pts, 2 * size))
         return 1;
-      else
-        size *= 2;
+      size *= 2;
     }
   }
 
   if (pts->n != size)
-    if (realloc_pts_failed (pts, pts->n))
+    if (realloc_pts_failed(pts, pts->n))
       return 1;
 
   return 0;
